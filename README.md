b400483b6c1fb393fd16b371eef376cc34b76cb7
Mateus Arce <mateusarce.developer@gmail.com>
2022-06-25 15:33:53 -0400

    finished project

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# lista_de_presen-a
+<h1 align="center">
+        Lista de Presença
+</h1>
+
+<h1 align="center">
+    <img src="img/capa.png">
+</h1>
+
+<h3 align="center">Você pode acessar o projeto <a href="https://bio-social-medias.vercel.app/" target="_blank">clicando aqui</a></h3>
+
+---
+
+</br>
+
+# Sobre o projeto
+
+**Bio Social-Medias** é um projeto que fiz para pôr na minha bio das redes sociais, semelhante ao linketree e ao beacons.
+</br>
+</br>
+<br/>
+
+# Tecnologias
+
+- [Html](https://www.w3schools.com/html/)
+- [Css](https://www.w3schools.com/css/)
+
+---
+<br/>
+
+## Como baixar o projeto
+
+```bash
+# Clonar o repositório
+$ git clone https://github.com/mateusarcedev/bio-social-medias
+# Entrar no diretório
+$ cd bio-social-medias
+```
+
+---
+
+<p align="center"> Desenvolvido por <a href="https://www.linkedin.com/in/mateus-arce/">Mateus Arce</a> ✌🏼</p>
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+
+import './style.css'
+
+import { Card } from '../../components/Card';
+
+export function Home() {
+  
+  const [studentName, setStudentName] = useState(); // fist param: the state name, second param: function that initializes the state
+  const [ students, setEstudents ] = useState([]);
+  const [user, setUser] = useState({ name: '', avatar: ''})
+
+  function handleAddStudent() {
+    const newStudent = {
+      name: studentName,
+      time: new Date().toLocaleTimeString("pt-br", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      })
+    };
+
+    setEstudents(prevState => [...prevState, newStudent]);
+  }
+
+  useEffect(() => {
+    //Corpo do useEffect -> ações que devem ser executadas
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/mateusarcedev')
+      const data = await response.json()
+       setUser({
+        name: data.name,
+        avatar: data.avatar_url
+    })
+    }
+
+    fetchData();
+
+  }, []);
+
+    return (
+    <div className="container"> 
+
+    <header>
+    <h1>Lista de Presença </h1>
+
+    <div>
+      <strong>{ user.name }</strong>
+      <img src={ user.avatar } alt="Profile Photo" />
+    </div>
+
+    </header>
+    
+    <input
+     type="text" 
+     placeholder="Digite o nome..."
+     onChange={e => setStudentName(e.target.value)}
+     />
+
+    <button type="button" onClick={handleAddStudent}>
+    Adicionar
+    </button>
+
+      {
+        students.map(student => (
+        <Card
+        key={student.time}
+        name={student.name} 
+        time={student.time} 
+        />))
+      }
+
+    </div>
+  )
+}
